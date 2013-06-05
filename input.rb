3fc31ea9b9328390167f43c414e1dda93e876a60
vejuhust <vejuhust@gmail.com>
2013-06-06 02:31:25 +0800

    not Fixnum bug fixed
    should use .collect, not .each

--- input.rb
+++ input.rb
@@ -5,8 +5,8 @@ def data_input(filename)
     lines = File.open(filename, "r").readlines
     $n_physical_node = Integer(lines[0])
     $m_virtual_node = Integer(lines[1])
-    $c_capicity = Array.new($n_physical_node) {|i| lines[i+2].split().each {|s| Integer(s)}}
-    $d_demands = lines[2+$n_physical_node].split().each {|s| Integer(s)}
+    $c_capicity = Array.new($n_physical_node) {|i| lines[i+2].split().collect {|s| Integer(s)}}
+    $d_demands = lines[2+$n_physical_node].split().collect {|s| Integer(s)}
 end
 
 data_input("input.txt")
