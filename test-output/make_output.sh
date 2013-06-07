ef472c78af98eafe01a026168099cef073842a2d
vejuhust <vejuhust@gmail.com>
2013-06-08 03:55:51 +0800

    test-output done
    bash shell script to run standard.out and generate output_data

---
+++ test-output/make_output.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+INDATADIR=input_data
+OUTDATADIR=output_data
+
+if [ $# -ne 1 ];
+then
+    app=./standard.out
+    printf "no arguments.\n"
+else
+    app=$1
+fi
+printf "'%s' will be executed to generate output.\n" "$app"
+
+mkdir "$OUTDATADIR"
+
+test_index=0
+for infile in "$INDATADIR"/input*.txt
+do
+    outfile=${infile//input/output}
+    cp -f "$infile" input.txt
+    "$app" > "$outfile"
+    printf "[Output %d: %s] %s\n" "$test_index" "$infile" "$result"
+    test_index=$[$test_index + 1]
+done
+
+rm -f input.txt
+
+exit 0
